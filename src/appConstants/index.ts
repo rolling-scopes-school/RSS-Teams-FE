3e51b72991392d03c5ff5edde8f16552bfc429d6
Sergei Nepryakhin <sergey.nepryahin@dashbouquet.com>
2021-02-09 18:15:17 +0300

    chore: fix import, fix team length, fix eslint rules, try to add eslintcache to gitignore one more time

--- src/appConstants/index.ts
+++ src/appConstants/index.ts
@@ -1,4 +1,4 @@
-import { BACKEND_LINK } from './api';
+export { BACKEND_LINK } from './api';
 
 export const SET_USER_DATA = 'SET_USER_DATA';
 export const SET_TEAMS = 'SET_TEAMS';
@@ -6,5 +6,4 @@ export const AUTH_TOKEN = 'AUTH_TOKEN';
 export const SET_TOKEN = 'SET_TOKEN';
 
 export const USERS_PER_PAGE = 40;
-export const TEAMS_PER_PAGE = 40;
-export { BACKEND_LINK };
+export const TEAMS_PER_PAGE = 10;
