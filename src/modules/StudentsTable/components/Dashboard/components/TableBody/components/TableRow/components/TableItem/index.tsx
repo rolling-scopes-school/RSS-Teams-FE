849fd48617cd63bd89b8d152b05c8624f00b1791
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-02-15 20:22:17 +0300

    RSSFE-08 feat: add react-window List

---
+++ src/modules/StudentsTable/components/Dashboard/components/TableBody/components/TableRow/components/TableItem/index.tsx
@@ -0,0 +1,45 @@
+import React, { FC, MouseEvent, useState } from 'react';
+import { TablePopup } from 'components/TablePopup';
+import { StyledTableItem } from './styled';
+
+type TableItemProps = {
+  item: string;
+  index: number;
+  dataLength: number;
+};
+
+export const TableItem: FC<TableItemProps> = ({ item, index, dataLength }) => {
+  const [tableItemCursor, setTableItemCursor] = useState<boolean>(false);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [popupElements, setPopupElements] = useState<string[]>([]);
+
+  const mouseOverHandler = (event: MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLDivElement;
+    if (target.scrollWidth !== target.clientWidth) {
+      setShowPopup(true);
+      setPopupElements(target?.textContent?.split(',') as string[]);
+      setTableItemCursor(true);
+    }
+  };
+
+  const mouseLeaveHandler = () => {
+    setShowPopup(false);
+    setPopupElements([]);
+    setTableItemCursor(false);
+  };
+
+  return (
+    <StyledTableItem
+      className={`TableItem--${index}`}
+      key={`TableItemKey-${index}`}
+      onMouseOver={mouseOverHandler}
+      onMouseOut={mouseLeaveHandler}
+      tableItemCursor={tableItemCursor}
+    >
+      <div className={`TableItem__first-element`}>{item}</div>
+      {showPopup ? (
+        <TablePopup popupElements={popupElements} dataLength={dataLength} />
+      ) : null}
+    </StyledTableItem>
+  );
+};
--- src/modules/StudentsTable/components/Dashboard/styled.ts
+++ src/modules/StudentsTable/components/Dashboard/styled.ts
@@ -1,35 +1,36 @@
 import styled from 'styled-components';
 import { WHITE_COLOR } from 'appConstants/colors';
 
-export const StyledTable = styled.table`
+export const StyledTable = styled.div`
   display: flex;
   flex-direction: column;
-  width: 92%;
+  width: 100%;
   max-width: 1320px;
-  height: calc(100vh - 220px);
+  height: calc(100vh - 345px);
   margin: 0 auto;
   padding: 10px;
+  padding-right: 0;
   font-size: 1rem;
   background-color: ${WHITE_COLOR};
   border-radius: 20px;
   @media (max-width: 1199px) and (min-width: 992px) {
+    height: calc(100vh - 320px);
     font-size: 0.95rem;
-    height: calc(100vh - 205px);
   }
   @media (max-width: 991px) and (min-width: 768px) {
+    height: calc(100vh - 295px);
     font-size: 0.9rem;
-    height: calc(100vh - 195px);
   }
   @media (max-width: 767px) and (min-width: 550px) {
+    height: calc(100vh - 270px);
     font-size: 0.825rem;
-    height: calc(100vh - 185px);
   }
   @media (max-width: 549px) and (min-width: 440px) {
+    height: calc(100vh - 260px);
     font-size: 0.8rem;
-    height: calc(100vh - 192px);
   }
   @media (max-width: 439px) and (min-width: 320px) {
+    height: calc(100vh - 220px);
     font-size: 0.68rem;
-    height: calc(100vh - 177px);
   }
 `;
