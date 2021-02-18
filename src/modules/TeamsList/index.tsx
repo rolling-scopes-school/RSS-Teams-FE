ff7f8c591c3da71d65be15b7bdd2e5a89cfec6ea
Alex Malagor <seo.dnja@gmail.com>
2021-02-18 20:46:52 +0300

    refactor: deleted modal on Teams Page

--- src/modules/TeamsList/index.tsx
+++ src/modules/TeamsList/index.tsx
@@ -1,21 +1,17 @@
 import React, { FC, useState } from 'react';
 import { useTeamsQuery } from 'hooks/graphql';
-import { Loader, Error, Modal, Pagination } from 'components';
+import { Loader, Error, Pagination } from 'components';
 import { useSelector } from 'react-redux';
 import { selectUserData } from 'modules/StudentsTable/selectors';
 import { selectCurrCourse } from 'modules/LoginPage/selectors';
 import { Teams } from './components/Teams';
 import { StyledTeams } from './styled';
-import { Button } from 'typography';
 import { TEAMS_PER_PAGE } from 'appConstants';
 
 export const TeamsList: FC = () => {
   const [page, setPage] = useState<number>(0);
   const currCourse = useSelector(selectCurrCourse);
   const userData = useSelector(selectUserData);
-  const [modalOpened, setModalOpened] = useState<boolean>(false);
-  const hideModal = () => setModalOpened(false);
-  const showModal = () => setModalOpened(true);
 
   const { loadingT, errorT, teams } = useTeamsQuery({
     reactCourseId: currCourse.id,
@@ -32,17 +28,10 @@ export const TeamsList: FC = () => {
     <>
       <StyledTeams>
         <Teams teams={teams} myTeam={teams.results[0]} userId={userData.id} />
-        <Pagination pageCount={pageCount} changePage={setPage} page={page} />
-        <Button type="button" onClick={showModal}>
-          Open modal
-        </Button>
+        {teams.count !== 0 && (
+          <Pagination pageCount={pageCount} changePage={setPage} page={page} />
+        )}
       </StyledTeams>
-      <Modal open={modalOpened} onClose={hideModal}>
-        <div>
-          <div>Title</div>
-          <div>Text</div>
-        </div>
-      </Modal>
     </>
   );
 };
