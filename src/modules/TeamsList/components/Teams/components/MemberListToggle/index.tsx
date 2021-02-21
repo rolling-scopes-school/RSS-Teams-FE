f4798b6dd093ad35fa4166c8c3b30c0eeac68f25
Alex Malagor <seo.dnja@gmail.com>
2021-02-21 11:44:21 +0300

    fix: return members arrows

--- src/modules/TeamsList/components/Teams/components/MemberListToggle/index.tsx
+++ src/modules/TeamsList/components/Teams/components/MemberListToggle/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { MembersListToggleStyled, Chevron } from './styled';
+import { ReactComponent as ChevronArrow } from 'assets/svg/chevron-arrow.svg';
 
 type MembersListToggle = {
   countMembers: number;
@@ -24,7 +25,9 @@ export const MembersListToggle: FC<MembersListToggle> = ({
       <div>
         {countMembers || 0} {countMembers === 1 ? 'member' : 'members'}
       </div>
-      <Chevron open={isOpen} />
+      <Chevron open={isOpen}>
+        <ChevronArrow />
+      </Chevron>
     </MembersListToggleStyled>
   );
 };
