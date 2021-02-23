fca5df7b00eeeac65c8a4610493317b07c543f2e
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2021-02-23 20:17:26 +0300

    feat(#RSSFE-37): update pre-build script - change BE url

--- pre-build.js
+++ pre-build.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 require('dotenv').config();
 
-const BACKEND_LINK = `export const BACKEND_LINK = 'https://rss-teams.herokuapp.com/graphql';
+const BACKEND_LINK = `export const BACKEND_LINK = 'https://rss-teams-305606.ey.r.appspot.com/graphql';
 `;
 
-const AUTH_BACKEND_LINK = `export const AUTH_BACKEND_LINK = 'https://rss-teams.herokuapp.com/auth/github/';
+const AUTH_BACKEND_LINK = `export const AUTH_BACKEND_LINK = 'https://rss-teams-305606.ey.r.appspot.com/auth/github/';
 `;
 
 fs.writeFileSync('./src/appConstants/api.ts', BACKEND_LINK + AUTH_BACKEND_LINK);
