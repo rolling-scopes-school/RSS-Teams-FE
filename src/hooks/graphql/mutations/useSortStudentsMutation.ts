483a5b6bf819a59e3eee72f438f95c767f026ce5
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-02-24 11:31:25 +0300

    RSSFE-16 feat: add refetchQuery

--- src/hooks/graphql/mutations/useSortStudentsMutation.ts
+++ src/hooks/graphql/mutations/useSortStudentsMutation.ts
@@ -1,18 +1,38 @@
 import { useMutation } from '@apollo/client';
+import { TEAMS_PER_PAGE } from 'appConstants';
 import { SORT_STUDENTS_MUTATION } from 'graphql/mutations';
+import { TEAMS_QUERY } from 'graphql/queries';
 
 type Props = {
   courseId: string;
+  page: number;
 };
 
-export const useSortStudentsMutation = ({ courseId }: Props) => {
-  const [sortStudents, { loading }] = useMutation(SORT_STUDENTS_MUTATION, {
-    variables: {
-      courseId,
-    },
-  });
+export const useSortStudentsMutation = ({ courseId, page }: Props) => {
+  const [sortStudents, { loading, data, error }] = useMutation(
+    SORT_STUDENTS_MUTATION,
+    {
+      variables: {
+        courseId,
+      },
+      refetchQueries: [
+        {
+          query: TEAMS_QUERY,
+          variables: {
+            courseId,
+            pagination: {
+              skip: page * TEAMS_PER_PAGE,
+              take: TEAMS_PER_PAGE,
+            },
+          },
+        },
+      ],
+    }
+  );
   return {
     sortStudents,
+    errorT: error,
+    teams: data?.teams,
     loadingM: loading,
   };
 };
