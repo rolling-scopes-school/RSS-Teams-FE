32feea06a3e67510a30b1568cf132da27c20739f
dariavv <daria.sidorovich333@gmail.com>
2021-10-12 20:20:58 +0200

    RSSFE-50 chore: remove console.dir

--- src/hooks/graphql/useOutsideClick.ts
+++ src/hooks/graphql/useOutsideClick.ts
@@ -3,7 +3,6 @@ import { RefObject, useEffect } from 'react';
 export const useOutsideClick = (ref: RefObject<HTMLElement>, callback: (e: MouseEvent) => void) => {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      console.dir(e.target);
       if (ref.current && !ref.current.contains(e.target as Node)) {
         callback(e);
       }
